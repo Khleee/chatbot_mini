5ae44aee5e3012f1212e71b0b8c85baaf332be0d
ekdma7077 <ekdma7913@innopost21.com>
2022-11-04 09:52:36 +0900

    플라스크 챗봇 추가

---
+++ chatbot2/README.md
@@ -0,0 +1,5 @@
+# Dongwon_chatbot_real
+
+## model
+다음 주소로 이동해서 폴더째로 받아가세요
+>\\Kict-nas\데이터사업추진팀\99. 개인폴더\유민석\2022_동원챗봇\intents_모델\kobert2
---
+++ chatbot2/static/js/main.js
@@ -0,0 +1,69 @@
+function sendMessage(text, type) {
+    $('.write_msg').val('');
+    if (type == 'bot'){
+        $('.msg_history').append('<div class="incoming_msg"><div class="incoming_msg_img"><img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"></div> <div class="received_msg"><div class="received_withd_msg"><p>'+text+'</p><span class="time_date">11:01 AM    |    June 9</span></div></div></div>')
+    }
+    else if(type == 'user'){
+        $('.msg_history').append('<div class="outgoing_msg"><div class="sent_msg"><p>'+text+'</p><span class="time_date">11:01 AM    |    June 9</span></div></div>')
+    }
+}
+
+function requestChat(messageText, okay, ending1,ending2,ending3,ending4, start, url_pattern) {
+    $.ajax({
+        url: "http://172.30.1.228:8080/" + url_pattern,
+        type: "POST",
+        dataType: "json",
+        data: {
+            'messageText': messageText,
+            'okay': okay,
+            'ending1': ending1,
+            'ending2': ending2,
+            'ending3': ending3,
+            'ending4': ending4,
+            'start': start
+        },
+        success: function (data) {
+            console.log(data)
+            if (data['type']=='bot'){
+                sendMessage(data['text'], 'bot'); 
+                $('.okay').attr('value', data['okay'])
+                $('.ending1').attr('value', data['ending1'])
+                $('.ending2').attr('value', data['ending2'])
+                $('.ending3').attr('value', data['ending3'])
+                $('.ending4').attr('value', data['ending4'])
+                $('.start').attr('value', data['start'])
+                $('.msg_history').scrollTop($(document).height());
+            }
+        },
+
+        error: function (request, status, error) {
+            console.log(error);
+            return sendMessage('죄송합니다. 서버 연결에 실패했습니다.', 'bot');
+        }
+    });
+}
+
+function greet() {
+    setTimeout(function () {
+        return sendMessage("동원챗봇 데모에 오신걸 환영합니다.", 'bot');
+    }, 100);
+}
+
+function onSendButtonClicked() {
+    let messageText = $('.write_msg').val();
+    let okay = $('.okay').val();
+    let ending1 = $('.ending1').val();
+    let ending2 = $('.ending2').val();
+    let ending3 = $('.ending3').val();
+    let ending4 = $('.ending4').val();
+    let start = $('.start').val();
+    if (messageText.length!=0)
+        sendMessage(messageText, 'user');
+        requestChat(messageText, okay, ending1,ending2,ending3,ending4, start, 'request_chat');
+}
+
+function onClickAsEnter(e) {
+    if (e.keyCode === 13 && $('.write_msg').val().length!=0) {
+        onSendButtonClicked()
+    }
+}
